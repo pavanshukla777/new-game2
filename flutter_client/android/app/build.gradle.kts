0ec1456018e963c098bb43455f699b4aebb2971a
pavanshukla777 <pavanshukla777@gmail.com>
2026-08-09 16:37:17 +0530

    Update build.gradle.kts

--- flutter_client/android/app/build.gradle.kts
+++ flutter_client/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     compileSdk = flutter.compileSdkVersion
     buildToolsVersion = "35.0.0"
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.3.13750724"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
